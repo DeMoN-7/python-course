42e9e30764a1e01ec4df58f955649c16791a8912
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2023-05-04 01:25:53 +0530

    defination

---
+++ After Mid sem/FunctionPractice1.py
@@ -0,0 +1,21 @@
+# basic function defination
+
+def a():
+    print("Hello")
+
+def b(a):
+    print("Hello: ",a)
+
+def c():
+
+    return 25
+def d(a):
+    return a*a
+
+
+a()
+b(20)
+c=c()
+print(c)
+d=d(2)
+print(d)
